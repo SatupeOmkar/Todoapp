bcdd273f0a8c9372d6b05c6da2586812d1dbe3ac
SatupeOmkar <omkarsatupe@gmail.com>
2023-06-14 18:48:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import React, { useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import { DragDropContext } from "react-beautiful-dnd";
+import List from "./List";
+import Alert from "./Alert";
+import { useGlobalContext } from "./context";
+import Colors from "./Colors";
+
+const App = () => {
+  const {
+    inputRef,
+    tasks,
+    setTasks,
+    alert,
+    showAlert,
+    isEditing,
+    setIsEditing,
+    editId,
+    setEditId,
+    name,
+    setName,
+    filter,
+    setFilter,
+    isColorsOpen,
+    setIsColorsOpen,
+  } = useGlobalContext();
+
+  const addTask = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "Invalid Task !");
+    } else if (name && isEditing) {
+      setTasks(
+        tasks.map((task) => {
+          return task.id === editId ? { ...task, name: name } : task;
+        })
+      );
+      setIsEditing(false);
+      setEditId(null);
+      setName("");
+      showAlert(true, "Task Edited Successfully.");
+    } else {
+      const newTask = {
+        id: uuid().slice(0, 8),
+        name: name,
+        completed: false,
+        color: "#009688",
+      };
+      setTasks([...tasks, newTask]);
+      showAlert(true, "Task Added Successfully.");
+      setName("");
+    }
+  };
+
+  const filterTasks = (e) => {
+    setFilter(e.target.dataset["filter"]);
+  };
+
+  const deleteAll = () => {
+    setTasks([]);
+    showAlert(true, "Your list is clear!");
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [inputRef, tasks]);
+
+  const handleDragEnd = (param) => {
+    const srcI = param.source.index;
+    const desI = param.destination?.index;
+    if (desI) {
+      const reOrdered = [...tasks];
+      reOrdered.splice(desI, 0, reOrdered.splice(srcI, 1)[0]);
+      setTasks(reOrdered);
+    }
+  };
+
+  const hideColorsContainer = (e) => {
+    //   body.
+    if (e.target.classList.contains("btn-colors")) return;
+    setIsColorsOpen(false);
+  };
+
+  return (
+  <>
+    <div className='container' onClick={hideColorsContainer}>
+    <h1> To-Do List</h1><br/>
+    <h3> "Organize your day, the easy way"</h3><br/>
+      {isColorsOpen && <Colors />}
+      {alert && <Alert msg={alert.msg} />}
+      <form className='head' onSubmit={addTask}>
+        <input
+          type='text'
+          ref={inputRef}
+          placeholder='New Task'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type='submit'>{isEditing ? "Edit" : "Add"}</button>
+      </form>
+      <div className='filter'>
+        <button
+          data-filter='all'
+          className={filter === "all" ? "active" : ""}
+          onClick={filterTasks}
+        >
+          All
+        </button>
+        <button
+          data-filter='complete'
+          className={filter === "complete" ? "active" : ""}
+          onClick={filterTasks}
+        >
+          Complete
+        </button>
+        <button
+          data-filter='incomplete'
+          className={filter === "incomplete" ? "active" : ""}
+          onClick={filterTasks}
+        >
+          Incomplete
+        </button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {tasks.length > 0 ? (
+          <List />
+        ) : (
+          <p className='no-tasks'>Your list is clear!</p>
+        )}
+      </DragDropContext>
+      {tasks.length > 2 && (
+        <button
+          className='btn-delete-all'
+          onClick={deleteAll}
+          title='Delete All Tasks (Complete and Incomplete)!'
+        >
+          Clear All
+        </button>
+      )}
+	  
+    </div>
+	</>
+  );
+};
+
+export default App;
+
+
